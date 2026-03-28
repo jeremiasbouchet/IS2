d987c5993da7e19b45a80ce94992246168e4ed02
jeremiasbouchet <jeremiasbouchet19@gmail.com>
2026-03-28 17:00:51 -0300

    modificando factorial.py 3

--- src/Factorial/factorial.py
+++ src/Factorial/factorial.py
@@ -18,22 +18,30 @@ def factorial(num):
         return fact 
 
 if len(sys.argv) < 2:
-    entrada = input("Ingrese número desde el 4 al 8: ")
+    entrada = input("Ingrese número o rango (ej: 4-8, -10, 10-): ")
 else:
     entrada = sys.argv[1]
 
-
 if "-" in entrada:
     partes = entrada.split("-")
-   
-    if partes[0] != "" and partes[1] != "":
+    
+    if partes[0] == "":
+        desde = 1
+        hasta = int(partes[1])
+
+    elif partes[1] == "":
+        desde = int(partes[0])
+        hasta = 60
+
+    else:
         desde = int(partes[0])
         hasta = int(partes[1])
-        for i in range(desde, hasta + 1):
-            print(f"Factorial de {i}! es {factorial(i)}")
-    else:
-        print("Formato de rango no soportado.")
-else:
     
+
+    for i in range(desde, hasta + 1):
+        print(f"Factorial de {i}! es {factorial(i)}")
+
+else:
+
     num = int(entrada)
     print(f"Factorial de {num}! es {factorial(num)}")
